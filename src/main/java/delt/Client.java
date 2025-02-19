d39ea2bc24b823958d36f79b7bf678834497794f
Altair <xamdiego2@hotmail.com>
2025-02-19 20:38:04 +0100

    Init

---
+++ src/main/java/delt/Client.java
@@ -0,0 +1,13 @@
+package delt;
+
+import org.teavm.jso.dom.html.HTMLDocument;
+
+public class Client {
+
+    public static void main(String[] args) {
+        var document = HTMLDocument.current();
+        var div = document.createElement("div");
+        div.appendChild(document.createTextNode("TeaVM generated element"));
+        document.getBody().appendChild(div);
+    }
+}
